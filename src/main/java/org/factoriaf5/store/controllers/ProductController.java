058a5f587ac5f8703da47df9aaf69991184bf464
eliana solsona <82371340+elianasolsona@users.noreply.github.com>
2021-05-17 21:37:39 +0200

    Add files via upload

---
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package org.factoriaf5.store.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+  @GetMapping("/health")
+  public String check() {
+    String greeting = "Hello there!";
+    return greeting;
+  }
+};
+
+
+
